2d3157574a59a491c8686c1d0f397fc6ea97e1b6
idi92 <edoardo.bellone@inaf.it>
2024-01-26 12:06:55 +0100

    update demo240124_spost-due_tilt: comparing measures with the ones taken controlling the slm via blink.exe

--- slm_4lgs_prototype/diffractive_spots/demo240124_spots_due_tilt.py
+++ slm_4lgs_prototype/diffractive_spots/demo240124_spots_due_tilt.py
@@ -2,7 +2,7 @@ import numpy as np
 from plico_dm import deformableMirror
 from slm_4lgs_prototype.diffractive_spots.spots_due_tilt import SpotPositionMeasurer
 from slm_4lgs_prototype.camera_controller.cblue1_controller import CBlueOneCamera
-
+from astropy.io import fits
 
 def main(lambda_vector, texp, gain, fps, Nframes, file_name):
     #creating devices
@@ -49,3 +49,26 @@ def show_images(file_name):
         plt.clf()
         plt.plot(cube_images[i].sum(axis=0)/cube_images[i].shape[0])
         plt.title("lambda = %g"%ptv_vector[i])
+        
+
+def get_image(texp, gain, fps, Nframes, file_name, wl = 589e-9):
+    camera = CBlueOneCamera("CBLUE1:MatroxCXP-Dev_0")
+    
+    fdir = "D:\\06 SLM\\diffractive_spots_res\\"
+    camera.set_fps(fps)
+    camera.set_exposure_time(texp)
+    camera.set_gain(gain)
+    
+    cube_image = camera.get_cube_of_raw_images(Nframes)
+    red_ima = np.median(cube_image, axis = 0)
+    camera.close_camera()
+    hdr = fits.Header()
+    hdr['T_EX_MS'] = texp
+    hdr['N_AV_FR'] = Nframes
+    hdr['G_DB'] = gain
+    hdr['WL_M'] = wl
+    hdr['FPS'] = fps
+    fits.writeto(fdir + file_name, red_ima, hdr, overwrite=True)
+    
+    
+    
